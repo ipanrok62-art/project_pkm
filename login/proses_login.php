09abd33306040b99710a43f7e889dfb39edc3b16
ipan <ipanrok62@gmail.com>
2026-05-23 08:26:14 +0700

    udah beres

--- login/proses_login.php
+++ login/proses_login.php
@@ -2,51 +2,56 @@
 
 session_start();
 
-$conn = mysqli_connect("localhost","root","","perpustakaan");
+// KONEKSI DATABASE
+$conn = mysqli_connect("localhost", "root", "", "perpustakaan");
 
+// CEK KONEKSI
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// AMBIL INPUT
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$query = mysqli_query($conn, "
+// QUERY LOGIN
+$sql = "SELECT * FROM `user` WHERE username='$username' AND password='$password'";
 
-SELECT * FROM 'user'
+$query = mysqli_query($conn, $sql);
 
-WHERE username='$username'
-
-AND password='$password'
-
-");
-
-$cek = mysqli_num_rows($query);
-
-if($cek > 0){
+// CEK LOGIN
+if (mysqli_num_rows($query) > 0) {
 
     $user = mysqli_fetch_assoc($query);
 
     $_SESSION['login'] = true;
-    $_SESSION['role'] = $user['role'];
     $_SESSION['username'] = $user['username'];
+    $_SESSION['role'] = $user['role'];
 
     // JIKA ADMIN
-    if($user['role'] == "admin"){
+    if ($user['role'] == "admin") {
 
         header("Location: ../admindashboard/admindashboard.php");
+        exit;
 
-    }
-
-    // JIKA PENGGUNA
-    else{
+    } 
+    
+    // JIKA USER
+    else {
 
         header("Location: ../dashboard/dashboard.php");
+        exit;
 
     }
 
-}else{
+} else {
 
-    echo "<script>
-    alert('Username atau Password salah!');
-    window.location.href = 'login.php';
-    </script>";
+    echo "
+    <script>
+        alert('Username atau Password salah!');
+        window.location.href='login.php';
+    </script>
+    ";
 
 }
 
